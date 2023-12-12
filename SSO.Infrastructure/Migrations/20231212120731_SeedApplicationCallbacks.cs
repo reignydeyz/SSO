dfa4f2fe7f8cc13ccd4ad44f24ac7652eb1f9acb
Alvin <actchua@periapsys.com>
2023-12-12 20:37:28 +0800

    SeedApplicationCallbacks

---
+++ SSO.Infrastructure/Migrations/20231212120731_SeedApplicationCallbacks.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SSO.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedApplicationCallbacks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "ApplicationCallbacks",
+                columns: new[] { "ApplicationId", "Url" },
+                values: new object[] { new Guid("69f900c3-dc6a-44e6-9988-50bba13542c6"), "https://periapsys.com" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
