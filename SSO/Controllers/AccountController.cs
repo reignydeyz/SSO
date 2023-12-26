24ee47bd902b95f1605423ab43caf073137d97b5
Alvin <actchua@periapsys.com>
2023-12-26 09:48:56 +0800

    Other controllers

---
+++ SSO/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SSO.Business.Accounts.Commands;
+using SSO.Domain.Management.Interfaces;
+using System.Security.Claims;
+
+namespace SSO.Controllers
+{
+    [ApiExplorerSettings(GroupName = "System")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class AccountController : ControllerBase
+    {
+        readonly IMediator _mediator;
+        readonly IUserRepository _userRepository;
+
+        public AccountController(IMediator mediator, IUserRepository userRepository)
+        {
+            _mediator = mediator;
+            _userRepository = userRepository;
+        }
+
+        /// <summary>
+        /// Changes password
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        [HttpPost("changepassword")]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordCommand param)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            param.User = await _userRepository.FindOne(x => x.Id == userId);
+
+            await _mediator.Send(param);
+
+            return Ok();
+        }
+    }
+}
---
+++ SSO/Controllers/AuthenticationController.cs
@@ -0,0 +1,125 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using SSO.Business.Authentication.Queries;
+using SSO.Filters;
+
+namespace SSO.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthenticationController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Request for app login
+        /// </summary>
+        /// <param name="form"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [AppIdValidator]
+        [ApiExplorerSettings(GroupName = "Client")]
+        [EnableCors("AllowAnyOrigin")]
+        public async Task<IActionResult> Init([FromQuery] InitLoginQuery form)
+        {
+            try
+            {
+                await _mediator.Send(form);
+
+                Response.Cookies.Append("appId", form.AppId!.Value.ToString(), new CookieOptions { Expires = DateTime.Now.AddDays(1), HttpOnly = false });
+
+                if (Request.Cookies["token"] != null)
+                {
+                    var token = await _mediator.Send(new SwitchAppQuery { Token = Request.Cookies["token"], AppId = form.AppId });
+
+                    Response.Cookies.Append("token", token.AccessToken, new CookieOptions { Expires = token.Expires, HttpOnly = false });
+
+                    return Redirect($"{form.CallbackUrl}?token={token.AccessToken}");
+                }
+
+                return Redirect($"{Request.Scheme}://{Request.Host}/login?appId={form.AppId}&callbackUrl={form.CallbackUrl}");
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Redirect($"{Request.Scheme}://{Request.Host}/login?appId={form.AppId}&callbackUrl={form.CallbackUrl}");
+            }
+        }
+
+        /// <summary>
+        /// Logout
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("logout")]
+        [EnableCors("AllowAnyOrigin")]
+        public IActionResult Logout()
+        {
+            Response.Cookies.Append("token", string.Empty, new CookieOptions { Expires = DateTime.Now.AddDays(-1), HttpOnly = false });
+            Response.Cookies.Append("system", string.Empty, new CookieOptions { Expires = DateTime.Now.AddDays(-1), HttpOnly = false });
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Authentication for app user
+        /// </summary>
+        /// <param name="form"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [AppIdValidator]
+        [ProducesResponseType(typeof(string), 200)]
+        [ApiExplorerSettings(GroupName = "System")]
+        public async Task<IActionResult> Login([FromBody] LoginQuery form)
+        {
+            try
+            {
+                var res = await _mediator.Send(form);
+
+                Response.Cookies.Append("token", res.AccessToken, new CookieOptions { Expires = res.Expires, HttpOnly = false });
+
+                return Ok(res);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Authentication for system
+        /// </summary>
+        /// <param name="form"></param>
+        /// <returns></returns>
+        [HttpPost("system")]
+        [ProducesResponseType(typeof(string), 200)]
+        [ApiExplorerSettings(GroupName = "System")]
+        public async Task<IActionResult> LoginToSystem([FromBody] LoginToSystemQuery form)
+        {
+            try
+            {
+                var res = await _mediator.Send(form);
+
+                Response.Cookies.Append("system", res.AccessToken, new CookieOptions { Expires = res.Expires, HttpOnly = false });
+
+                return Ok(res);
+            }
+            catch (ArgumentNullException)
+            {
+                return Unauthorized();
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+        }
+    }
+}
