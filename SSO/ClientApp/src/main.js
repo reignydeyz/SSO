29c86841b2c1948bb44b90bd52abdff65767275f
Alvin <actchua@periapsys.com>
2024-04-07 14:51:57 +0800

    Hotfix | Fix autocomplete css

--- SSO/ClientApp/src/main.js
+++ SSO/ClientApp/src/main.js
@@ -2,6 +2,7 @@ import '@popperjs/core/lib/popper.js'
 import "bootstrap-icons/font/bootstrap-icons.css"
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+import 'autocompleter/autocomplete.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
