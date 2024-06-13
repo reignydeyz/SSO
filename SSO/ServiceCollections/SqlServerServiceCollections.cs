d7d433e425c35be62259984c2a0a912292ea7bde
Alvin <actchua@periapsys.com>
2024-06-13 20:58:23 +0800

    Hotfix

--- SSO/ServiceCollections/SqlServerServiceCollections.cs
+++ SSO/ServiceCollections/SqlServerServiceCollections.cs
@@ -19,8 +19,8 @@ namespace SSO.ServiceCollections
                 .AddDefaultTokenProviders();
 
 #if !DEBUG
-using (var scope = builder.Services.BuildServiceProvider().CreateScope())
-{
+using (var scope = services.BuildServiceProvider().CreateScope())
+{ 
     var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     dbContext.Database.Migrate();
 }
