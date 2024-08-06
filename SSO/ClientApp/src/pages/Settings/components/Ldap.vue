00c32e91760c0cd184f80f06ead3c0ef7bbcd91f
Alvin <actchua@periapsys.com>
2024-08-06 12:21:16 +0800

    Load values

--- SSO/ClientApp/src/pages/Settings/components/Ldap.vue
+++ SSO/ClientApp/src/pages/Settings/components/Ldap.vue
@@ -11,27 +11,31 @@
                     <form class="settings-form">
                         <div class="mb-3">
                             <label for="setting-input-2" class="form-label">Server*</label>
-                            <input class="form-control" placeholder="Server" required autocomplete="off" />
+                            <input class="form-control" placeholder="Server" required autocomplete="off"
+                                v-model="ldapSettings.server" />
                         </div>
                         <div class="mb-3">
                             <label for="setting-input-2" class="form-label">Port*</label>
-                            <input class="form-control" type="number" required autocomplete="off" />
+                            <input class="form-control" type="number" required autocomplete="off" v-model="ldapSettings.port" />
                         </div>
                         <div class="mb-3">
                             <label for="setting-input-2" class="form-label">Username*</label>
-                            <input class="form-control" placeholder="Username" required autocomplete="off" />
+                            <input class="form-control" placeholder="Username" required autocomplete="off" v-model="ldapSettings.username" />
                         </div>
                         <div class="mb-3">
                             <label for="setting-input-2" class="form-label">Password*</label>
-                            <input class="form-control" type="password" required autocomplete="off" />
+                            <input class="form-control" type="password" required autocomplete="off" v-model="ldapSettings.password"/>
                         </div>
                         <div class="mb-3">
                             <label for="setting-input-2" class="form-label">Search base</label>
-                            <input class="form-control" autocomplete="off" />
+                            <input class="form-control" autocomplete="off" v-model="ldapSettings.searchBase"/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="setting-input-2" class="form-label">Search Filter</label>
+                            <input class="form-control" autocomplete="off" v-model="ldapSettings.searchFilter"/>
                         </div>
                         <div class="form-check mb-3">
-                            <input class="form-check-input" type="checkbox" value=""
-                                checked="">
+                            <input class="form-check-input" type="checkbox" value="" v-model="ldapSettings.useSSL">
                             <label class="form-check-label" for="settings-checkbox-1">
                                 Use SSL
                             </label>
@@ -49,5 +53,16 @@
 <script>
 export default {
     props: ["realm"],
+    data: () => ({
+        ldapSettings: new Object(),
+    }),
+    watch: {
+        realm(newVal, oldVal) {
+            let res = newVal.realmIdpSettingsCollection
+                .filter(x => x.identityProvider === 1);
+
+            this.ldapSettings = res.length > 0 ? res[0].value : new Object();
+        }
+    },
 }
 </script>
