b57a5e5901d0641edca114d770a54aa8e6f32a50
Alvin <actchua@periapsys.com>
2024-08-13 21:34:49 +0800

    Enhancements

--- SSO/ClientApp/src/pages/Settings/components/Ldap.vue
+++ SSO/ClientApp/src/pages/Settings/components/Ldap.vue
@@ -8,15 +8,18 @@
         <div class="col-12 col-md-9">
             <div class="app-card app-card-settings shadow-sm p-4">
                 <div class="app-card-body">
-                    <form class="settings-form" @submit.prevent="onSubmit">
+                    <form class="settings-form" @submit.prevent="onSubmit" ref="form">
+                        <div class="alert alert-warning mt-2" role="alert">
+                            <b>Warning:</b> Ensure that you create an admin user with the correct LDAP credentials first. Use this user to log in to the LDAP domain to verify proper access and functionality.
+                        </div>
                         <div class="mb-3">
                             <label for="setting-input-2" class="form-label">Server*</label>
-                            <input class="form-control" placeholder="Server" required autocomplete="off"
+                            <input class="form-control" placeholder="yourdomain.com" required autocomplete="off"
                                 v-model="ldapSettings.server" />
                         </div>
                         <div class="mb-3">
                             <label for="setting-input-2" class="form-label">Port*</label>
-                            <input class="form-control" type="number" required autocomplete="off"
+                            <input class="form-control" type="number" placeholder="389" required autocomplete="off"
                                 v-model="ldapSettings.port" />
                         </div>
                         <div class="mb-3">
@@ -30,27 +33,30 @@
                                 v-model="ldapSettings.password" />
                         </div>
                         <div class="mb-3">
-                            <label for="setting-input-2" class="form-label">Search base</label>
-                            <input class="form-control" autocomplete="off" v-model="ldapSettings.searchBase" />
+                            <label for="setting-input-2" class="form-label">Search base*</label>
+                            <input class="form-control" autocomplete="off" placeholder="dc=example,dc=com" required
+                                v-model="ldapSettings.searchBase" />
                         </div>
                         <div class="mb-3">
                             <label for="setting-input-2" class="form-label">Search Filter</label>
-                            <input class="form-control" autocomplete="off" v-model="ldapSettings.searchFilter" />
+                            <input class="form-control" autocomplete="off" placeholder="(objectClass=*)"
+                                v-model="ldapSettings.searchFilter" />
                         </div>
                         <div class="form-check mb-3">
                             <input class="form-check-input" type="checkbox" value="" v-model="ldapSettings.useSSL">
-                            <label class="form-check-label" for="settings-checkbox-1">
+                            <label class="form-label form-check-label" for="settings-checkbox-1">
                                 Use SSL
                             </label>
                         </div>
                         <div class="row">
-                            <div class="col-auto">
-                                <button type="submit" class="btn app-btn-primary">
-                            Save Changes
-                        </button>
+                            <div class="col-auto mt-2">
+                                <button type="submit" class="btn app-btn-primary">Save Changes</button>&nbsp;
+                                <button type="button" class="btn app-btn-outline-primary" @click="onTest()">Test
+                                    Settings</button>
                             </div>
-                            <div class="col-auto ms-auto">
-                                <button type="button" class="btn btn-danger" v-show="showDeleteButton" @click="onDelete()">Remove</button>
+                            <div class="col-auto ms-auto mt-2">
+                                <button type="button" class="btn btn-danger" v-show="showDeleteButton"
+                                    @click="onDelete()">Remove</button>
                             </div>
                         </div>
                     </form>
@@ -62,7 +68,7 @@
 
 <script>
 import { emitter } from "@/services/emitter.service";
-import { modifyLdapSetting, deleteLdapSetting } from "@/services/realm-idp-settings.service";
+import { modifyLdapSetting, deleteLdapSetting, testLdapSetting } from "@/services/realm-idp-settings.service";
 export default {
     props: ["realm"],
     data: () => ({
@@ -85,6 +91,7 @@ export default {
             modifyLdapSetting(this.ldapSettings).then(r => {
                 emitter.emit("showLoader", false);
                 this.showDeleteButton = true;
+                alert('Saved!');
             }, (err) => {
                 alert('Failed to update record.');
                 emitter.emit("showLoader", false);
@@ -96,10 +103,38 @@ export default {
                 deleteLdapSetting().then(r => {
                     window.location.reload();
                 }, err => {
-                    emitter.emit("showLoader", true);
+                    emitter.emit("showLoader", false);
+                });
+            }
+        },
+        onTest() {
+            if (this.$refs.form.reportValidity()) {
+                emitter.emit("showLoader", true);
+                testLdapSetting(this.ldapSettings).then(r => {
+                    emitter.emit("showLoader", false);
+                    alert('Connected!');
+                }, (err) => {
+                    emitter.emit("showLoader", false);
+                    alert('Failed to connect.');
                 });
             }
         }
     }
 }
 </script>
+
+<style scoped>
+@media (max-width: 768px) {
+
+    /* Adjust the max-width as needed for your mobile breakpoint */
+    .row {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+    }
+
+    .col-auto {
+        width: 100%;
+    }
+}
+</style>
--- SSO/ClientApp/src/services/realm-idp-settings.service.js
+++ SSO/ClientApp/src/services/realm-idp-settings.service.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const modifyLdapSetting = async (param) => await axios.post("api/realm/idp/ldap", param);
-const deleteLdapSetting = async () => await axios.delete(`/api/realm/idp/ldap`)
+const deleteLdapSetting = async () => await axios.delete(`/api/realm/idp/ldap`);
+const testLdapSetting = async (param) => await axios.post(`/api/realm/idp/ldap/test`, param)
 
 export {
     modifyLdapSetting,
-    deleteLdapSetting
+    deleteLdapSetting,
+    testLdapSetting
 }
